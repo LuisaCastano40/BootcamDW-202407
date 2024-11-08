d15a2de29f5a63197d758129b8f18de50cc364a8
LuisaCastano40 <luisa.castano40@eia.edu.co>
2024-11-07 21:41:53 -0500

    [actualizacion] loginService

--- MÓDULO2/proyectoDeClase/Backend/src/lib/jwt.js
+++ MÓDULO2/proyectoDeClase/Backend/src/lib/jwt.js
@@ -6,9 +6,48 @@
 
 // ---------------------------------------------------------------------------
 
-// Importar módulos y dependencias que necesitemos
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+// 3.Importar módulos y dependencias que necesitemos
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 
+// 4. configurar el uso de la clave secreta creada en el archivo .env
 const key = process.env.SECRET_KEY;
+
+// 5. Crear las funciones para generar y verificar el token
+
+// Función para generar token -> funciones declaradas o funciones flecha
+// payload -> información del usuario
+export function generateToken(payload) {
+  // nos estamos creando una función que promete devolver una respuesta después de cierto tiempo
+  // Estamos utilizando una función que se llama callback -> ()=>{} -> dedvuelve algo
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, key, { expiresIn: "1h" }, (error, token) => {
+      if (error) {
+        // si sale mal, que me responsa con un error
+        reject(new Error("Error al generar JWT " + error.message));
+      } else {
+        // Si sale bien, que me responsa con el token
+        resolve(token);
+      }
+    });
+  });
+}
+
+// Función para verficar que el token sea el generado por nuestro servidor y no fue alterado
+// token -> el token que queremos verificar
+export const verifyToken = (token) => {
+
+  return new Promise((resolve, reject) => {
+
+    jwt.verify(token, key, (error, decoded) => {
+      if (error) {
+        // si sale mal, que me responsa con un error
+        reject(new Error("Error al verificar JWT " + error.message));
+      }else{
+        resolve(decoded);
+      }
+
+    });
+  });
+}
