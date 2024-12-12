8e58e0a6da9afe668e37e72de4c4f7c27fb6cc29
LuisaCastano40 <luisa.castano40@eia.edu.co>
2024-12-11 21:43:47 -0500

    [actualizacion] html de componentes

--- MÓDULO3/proyectoDeClase/Fronted/src/app/componentes/nav-bar/nav-bar.component.ts
+++ MÓDULO3/proyectoDeClase/Fronted/src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+// para usar estructuras de control en html, debo importarlas
+import { NgIf } from '@angular/common';
 
 @Component({
   // es lo que usamos para llamar nuestro componente reutilizable en el HTML
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 
 // Exportamos el componente a otras paginas
 export class NavBarComponent {
+
+  // variable que busca guardar el estado del usuario como logueado o no
+  // Las estructuras de control ngIf y ngFor nos permiten manejar lógica del ts
+  // en el html
+  isLogged : boolean = true;
 
 }
--- MÓDULO3/proyectoDeClase/Fronted/src/app/paginas/productos/productos.component.ts
+++ MÓDULO3/proyectoDeClase/Fronted/src/app/paginas/productos/productos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../componentes/nav-bar/nav-bar.component';
+import { TarjetaProductoComponent } from '../../componentes/tarjeta-producto/tarjeta-producto.component';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, TarjetaProductoComponent],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
